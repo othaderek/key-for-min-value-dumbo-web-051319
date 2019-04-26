db99ceb2d76fcb46df1d06f2dc69d4e7c4c086ca
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-26 19:23:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,11 @@ def key_for_min_value(name_hash)
   key2, value2 = name_hash.shift
 
   if value < value1 && value < value2
-  	puts key
+  	puts key.to_sym
   elsif value1 < value && value1 < value2
-  	puts key1
+  	puts key1.to_sym
   elsif value2 < value && value2 < value1
-  	puts key2
+  	puts key2.to_sym
   end
 
 end

f46a52b489436995feddb7480904104ac0717cc1
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-26 19:32:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,9 +10,10 @@ def key_for_min_value(name_hash)
   if value < value1 && value < value2
   	solution = key
   elsif value1 < value && value1 < value2
-  	puts key1.to_sym
+  	solution = key1
   elsif value2 < value && value2 < value1
-  	puts key2.to_sym
+  	solution = key2
   end
+  puts solution
 
 end

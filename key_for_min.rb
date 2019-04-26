3e256d0e6886e97ad1ea01a969ae1beeb07d7f6f
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-26 20:07:48 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil 
+  end
+
+  key, value = name_hash.shift
+  key1, value1 = name_hash.shift
+  key2, value2 = name_hash.shift
+
+  if value < value1 && value < value2
+  	return :blake
+  elsif value1 < value && value1 < value2
+  	return :ashley
+  elsif value2 < value && value2 < value1
+  	return :adam
+  end
+  
+
+
 
 end

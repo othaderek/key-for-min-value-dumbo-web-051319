7ad36d2010b5836c4a296938d0ddbbf223c43fe1
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-26 19:52:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,15 @@ def key_for_min_value(name_hash)
   key, value = name_hash.shift
   key1, value1 = name_hash.shift
   key2, value2 = name_hash.shift
+  
+  if name_hash != {}
 
-  if value < value1 && value < value2
-  	return :blake
-  elsif value1 < value && value1 < value2
-  	return :ashley
-  elsif value2 < value && value2 < value1
-  	return :adam
-  end
+    if value < value1 && value < value2
+    	return :blake
+    elsif value1 < value && value1 < value2
+    	return :ashley
+    elsif value2 < value && value2 < value1
+    	return :adam
+    end
 
 end
